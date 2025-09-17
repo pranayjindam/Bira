9cbf396e4ef8f7b0f9edc8c0adcbe936967c6031
Bharath <kammalabharath96@gmail.com>
2025-09-17 18:16:08 +0530

    UI signup 6:16

--- Bira/Naveen/Signup.Designer.cs
+++ Bira/Naveen/Signup.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Bira.Naveen
+﻿using Bira.UI;
+
+namespace Bira.Naveen
 {
     partial class Signup
     {
@@ -54,142 +56,70 @@
             // 
             // FirstNameTxt
             // 
-            FirstNameTxt.AutoSize = true;
-            FirstNameTxt.Font = new Font("Segoe UI", 12F);
-            FirstNameTxt.ForeColor = Color.White;
+            FirstNameTxt = UIControls.UIControlFactory.CreateTextLabel("First Name");
             FirstNameTxt.Location = new Point(22, 126);
-            FirstNameTxt.Margin = new Padding(4, 0, 4, 0);
-            FirstNameTxt.Name = "FirstNameTxt";
-            FirstNameTxt.Size = new Size(129, 32);
-            FirstNameTxt.TabIndex = 1;
-            FirstNameTxt.Text = "First Name";
+            
             // 
             // SignupFirstNameTxtBox
             // 
-            SignupFirstNameTxtBox.Font = new Font("Segoe UI", 12F);
+            SignupFirstNameTxtBox = InputControls.InputControlFactory.CreateTextInput(placeholder: "Enter your first name");
             SignupFirstNameTxtBox.Location = new Point(268, 115);
-            SignupFirstNameTxtBox.Margin = new Padding(4);
-            SignupFirstNameTxtBox.Name = "SignupFirstNameTxtBox";
-            SignupFirstNameTxtBox.PlaceholderText = "Enter your first name";
-            SignupFirstNameTxtBox.Size = new Size(488, 39);
-            SignupFirstNameTxtBox.TabIndex = 2;
             // 
             // LastNameTxt
             // 
-            LastNameTxt.AutoSize = true;
-            LastNameTxt.Font = new Font("Segoe UI", 12F);
-            LastNameTxt.ForeColor = Color.White;
+            LastNameTxt = UIControls.UIControlFactory.CreateTextLabel("Last Name");
             LastNameTxt.Location = new Point(22, 211);
-            LastNameTxt.Margin = new Padding(4, 0, 4, 0);
-            LastNameTxt.Name = "LastNameTxt";
-            LastNameTxt.Size = new Size(126, 32);
-            LastNameTxt.TabIndex = 3;
-            LastNameTxt.Text = "Last Name";
             // 
             // SignupLastNameTxtBox
             // 
-            SignupLastNameTxtBox.Font = new Font("Segoe UI", 12F);
+            SignupLastNameTxtBox = InputControls.InputControlFactory.CreateTextInput(placeholder: "Enter your last name");
             SignupLastNameTxtBox.Location = new Point(268, 200);
-            SignupLastNameTxtBox.Margin = new Padding(4);
-            SignupLastNameTxtBox.Name = "SignupLastNameTxtBox";
-            SignupLastNameTxtBox.PlaceholderText = "Enter your last name";
-            SignupLastNameTxtBox.Size = new Size(488, 39);
-            SignupLastNameTxtBox.TabIndex = 4;
             // 
             // EmailTxt
             // 
-            EmailTxt.AutoSize = true;
-            EmailTxt.Font = new Font("Segoe UI", 12F);
-            EmailTxt.ForeColor = Color.White;
+            EmailTxt = UIControls.UIControlFactory.CreateTextLabel("Email");
             EmailTxt.Location = new Point(22, 298);
-            EmailTxt.Margin = new Padding(4, 0, 4, 0);
-            EmailTxt.Name = "EmailTxt";
-            EmailTxt.Size = new Size(71, 32);
-            EmailTxt.TabIndex = 5;
-            EmailTxt.Text = "Email";
             // 
             // SignupEmailTxtBox
             // 
-            SignupEmailTxtBox.Font = new Font("Segoe UI", 12F);
+            SignupEmailTxtBox = InputControls.InputControlFactory.CreateTextInput(placeholder: "Enter your email");
             SignupEmailTxtBox.Location = new Point(268, 286);
-            SignupEmailTxtBox.Margin = new Padding(4);
-            SignupEmailTxtBox.Name = "SignupEmailTxtBox";
-            SignupEmailTxtBox.PlaceholderText = "Enter your email";
-            SignupEmailTxtBox.Size = new Size(488, 39);
-            SignupEmailTxtBox.TabIndex = 6;
             // 
             // PasswordTxt
             // 
-            PasswordTxt.AutoSize = true;
-            PasswordTxt.Font = new Font("Segoe UI", 12F);
-            PasswordTxt.ForeColor = Color.White;
+            PasswordTxt = UIControls.UIControlFactory.CreateTextLabel("Password");
             PasswordTxt.Location = new Point(22, 391);
-            PasswordTxt.Margin = new Padding(4, 0, 4, 0);
-            PasswordTxt.Name = "PasswordTxt";
-            PasswordTxt.Size = new Size(111, 32);
-            PasswordTxt.TabIndex = 7;
-            PasswordTxt.Text = "Password";
             // 
             // SignupPasswordTxtBox
             // 
-            SignupPasswordTxtBox.Font = new Font("Segoe UI", 12F);
+            SignupPasswordTxtBox = InputControls.InputControlFactory.CreateTextInput(placeholder: "Enter your password");
             SignupPasswordTxtBox.Location = new Point(268, 380);
-            SignupPasswordTxtBox.Margin = new Padding(4);
-            SignupPasswordTxtBox.Name = "SignupPasswordTxtBox";
-            SignupPasswordTxtBox.PlaceholderText = "Enter your password";
-            SignupPasswordTxtBox.Size = new Size(488, 39);
-            SignupPasswordTxtBox.TabIndex = 9;
             SignupPasswordTxtBox.TextChanged += SignupPasswordTxt_TextChanged;
             // 
             // ConfirmPasswordTxt
             // 
-            ConfirmPasswordTxt.AutoSize = true;
-            ConfirmPasswordTxt.Font = new Font("Segoe UI", 12F);
-            ConfirmPasswordTxt.ForeColor = Color.White;
+            ConfirmPasswordTxt = UIControls.UIControlFactory.CreateTextLabel("Confirm Password");
             ConfirmPasswordTxt.Location = new Point(11, 485);
-            ConfirmPasswordTxt.Margin = new Padding(4, 0, 4, 0);
-            ConfirmPasswordTxt.Name = "ConfirmPasswordTxt";
-            ConfirmPasswordTxt.Size = new Size(204, 32);
-            ConfirmPasswordTxt.TabIndex = 10;
-            ConfirmPasswordTxt.Text = "Confirm Password";
             // 
             // SignupConfirmPasswordTxtBox
             // 
-            SignupConfirmPasswordTxtBox.Font = new Font("Segoe UI", 12F);
+            SignupConfirmPasswordTxtBox = InputControls.InputControlFactory.CreateTextInput(placeholder: "Confirm your password");
             SignupConfirmPasswordTxtBox.Location = new Point(268, 474);
-            SignupConfirmPasswordTxtBox.Margin = new Padding(4);
-            SignupConfirmPasswordTxtBox.Name = "SignupConfirmPasswordTxtBox";
-            SignupConfirmPasswordTxtBox.PlaceholderText = "Confirm your password";
-            SignupConfirmPasswordTxtBox.Size = new Size(488, 39);
             SignupConfirmPasswordTxtBox.TabIndex = 11;
             SignupConfirmPasswordTxtBox.TextChanged += SignupConfirmPasswordTxt_TextChanged;
             // 
             // SignUpBtn
             // 
-            SignUpBtn.BackColor = Color.DarkOrange;
-            SignUpBtn.FlatAppearance.MouseOverBackColor = Color.OrangeRed;
-            SignUpBtn.Font = new Font("Segoe UI", 16F);
+            SignUpBtn = UIControls.UIControlFactory.CreatePrimaryButton("Sign Up"); 
             SignUpBtn.Location = new Point(305, 603);
-            SignUpBtn.Margin = new Padding(4);
-            SignUpBtn.Name = "SignUpBtn";
-            SignUpBtn.Size = new Size(178, 58);
-            SignUpBtn.TabIndex = 12;
-            SignUpBtn.Text = "Sign Up";
             SignUpBtn.UseVisualStyleBackColor = false;
             SignUpBtn.Click += SignUpBtn_Click;
             // 
             // HaveAccTxt
             // 
-            HaveAccTxt.AutoSize = true;
-            HaveAccTxt.Font = new Font("Segoe UI", 10F);
-            HaveAccTxt.ForeColor = Color.White;
+            HaveAccTxt = UIControls.UIControlFactory.CreateTextLabel("Already have an account?");
             HaveAccTxt.Location = new Point(449, 549);
-            HaveAccTxt.Margin = new Padding(4, 0, 4, 0);
-            HaveAccTxt.Name = "HaveAccTxt";
-            HaveAccTxt.Size = new Size(234, 28);
             HaveAccTxt.TabIndex = 13;
-            HaveAccTxt.Text = "Already have an account?";
-            // 
             // SignupTransparentPanel
             // 
             SignupTransparentPanel.BackColor = Color.DarkTurquoise;
